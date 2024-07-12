e90311bc28bb77f057c6865cfcde45229b5b64eb
Enzo Busseti <enzo.busseti@me.com>
2024-07-13 02:32:16 +0400

    GPLv3

--- project_euromir/dcsrch.c
+++ project_euromir/dcsrch.c
@@ -1,41 +1,20 @@
 /*
-BSD 3-Clause License
+Copyright 2024 Enzo Busseti
 
-Copyright (c) 2024-, Enzo Busseti
+This file is part of Project Euromir.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+Project Euromir is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option) any
+later version.
 
-1. Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
+Project Euromir is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+details.
 
-2. Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
-
-3. Neither the name of the copyright holder nor the names of its
-   contributors may be used to endorse or promote products derived from
-   this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
-Some changes by hand to the f2c translation of the original FORTRAN code from
-Lbfgsb3.0. See copyright notice therein.
-
-We remove the task string and replace it with an integer return value:
-positive for valid tasks, negative for errors. Return 0 means the current
-point is valid (convergence). Return 1 means re-call the function with
-start=False and new point. Initially must be called with start=true, then
-with start=false.
+You should have received a copy of the GNU General Public License along with
+Project Euromir. If not, see <https://www.gnu.org/licenses/>.
 */
 
 
